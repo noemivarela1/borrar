a3463687b837d7243ed15576bf66f3b676694081
Noemi <noemivarela1@gmail.com>
2024-09-18 19:59:38 +0200

    New version of NewClass in dev branch

--- src/main/java/org/example/NewClass.java
+++ src/main/java/org/example/NewClass.java
@@ -8,6 +8,8 @@ public class NewClass {
         System.out.println("New Class with pid:"+pid);
 
         Scanner in=new Scanner(System.in);
-        in.nextInt();
+        System.out.println("Set a number:");
+        int number=in.nextInt();
+        System.out.println("the number is:"+number);
     }
 }
