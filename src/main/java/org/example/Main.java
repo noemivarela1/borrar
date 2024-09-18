5a48aeb3d85a25d5722be69585f5f369fb60d52a
Noemi <noemivarela1@gmail.com>
2024-09-18 19:50:05 +0200

    Last version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
+        System.out.println("Hello and welcome!");
+        long pid = ProcessHandle.current().pid();
+        System.out.println("New Cass with pid:"+pid);
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
--- src/main/java/org/example/NewClass.java
+++ src/main/java/org/example/NewClass.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("New Class");
+        long pid = ProcessHandle.current().pid();
+        System.out.println("New Class with pid:"+pid);
+
+        Scanner in=new Scanner(System.in);
+        in.nextInt();
     }
 }
